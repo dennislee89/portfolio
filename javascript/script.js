a80eaf54aa020ffca0467f1f520a64e969895bd6
dennislee89 <dennislee1989@gmail.com>
2017-01-08 21:41:36 -0600

    revised whole website

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+function flip(){
+  document.getElementById('card').classList.toggle('flip');
+}
+
+function flip2(){
+  document.getElementById('card2').classList.toggle('flip');
+}
+
+function flip3(){
+  document.getElementById('card3').classList.toggle('flip');
+}
